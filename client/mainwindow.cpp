cb0c90a51e95fb261d6f6267e5a4e5dcdd940254
Maksim Shishov <maks-shishov@mail.ru>
2015-09-14 14:50:44 +0300

    add .gitignore and add client

--- client/mainwindow.cpp
+++ client/mainwindow.cpp
@@ -6,17 +6,9 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    server->listen(QHostAddress::Any, 4729);
-    connect(server, SIGNAL(newConnection()), this, SLOT(addConnection()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
-}
-
-void MainWindow::addConnection()
-{
-    sock = new QTcpSocket();
 }
