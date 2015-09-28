243a155741ac48131ded7405493f6a455f8a7059
Maksim Shishov <maks-shishov@mail.ru>
2015-09-28 12:26:24 +0300

    fix

--- server/userscertificates.cpp
+++ server/userscertificates.cpp
@@ -4,6 +4,7 @@
 
 const QString USER_CERTS = "user_certificate/%1";
 
+//Users certificates path HOME$/.config/RBT/QSslServer.conf
 UsersCertificates::UsersCertificates() : m_settings("RBT", "QSslServer")
 {
     int i = 0;
@@ -24,10 +25,12 @@ UsersCertificates::UsersCertificates() : m_settings("RBT", "QSslServer")
 
 void UsersCertificates::add(const QByteArray &cert)
 {
-    m_settings.setValue(USER_CERTS.arg(m_clientsNum++), cert);
+    if (!contains(QSslCertificate(cert)))
+        m_settings.setValue(USER_CERTS.arg(m_clientsNum++), cert);
     qDebug() << Q_FUNC_INFO << m_clientsNum;
 }
 
+//certificate must be in Pem encoding.
 bool UsersCertificates::contains(const QSslCertificate &cert)
 {
     for (int i = 0; i < m_clientsNum; i++)
